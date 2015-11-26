45dd018b26599a3fa0b4504a1573399052a6aaf1
Antoine Drouhin <antoine.drouhin@gmail.com>
2015-11-26 19:54:20 +0100

    Première partie de la conception. Reste à concevoir le package pooTD4. Bisous

--- src/IndexDeMots/Index.java
+++ src/IndexDeMots/Index.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package IndexDeMots;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Antoine Drouhin, Anis Mezdari
  */
-public class Index {
+public class Index implements IntIndex {
+
+    @Override
+    public boolean ajoutMot(String k, int line) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String k, int line) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<String, List<Integer>> getIndex() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Integer> rechercher(String k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
